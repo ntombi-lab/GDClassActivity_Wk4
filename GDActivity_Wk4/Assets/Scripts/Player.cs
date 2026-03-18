993aab3655357719a5930458035d0d388ced86aa
ntombi-lab <ntombizanamuhla@gmail.com>
2026-03-18 16:57:36 +0200

    player basic input

---
+++ GDActivity_Wk4/Assets/Scripts/Player.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float speed;
+   
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.UpArrow)) 
+        {
+            Debug.Log("Pressed the Arrow Key");
+        }
+    }
+}
